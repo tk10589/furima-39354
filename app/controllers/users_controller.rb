869b1410b9030f29d604a8a518b5a832456439f7
tk10589 <k1taka2014@gmail.com>
2023-05-26 22:08:16 +0900

    新規登録・ログインエラーハンドリング実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,18 @@
 class UsersController < ApplicationController
   
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+    
+  private
+    
+  def user_params
+    params.require(:user).permit(:nickname, :fname, :lname, :fname_kana, :lname_kana, :birthdate)
+  end
+    
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,14 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   validates :nickname, presence: true
-  validates :fname, presence: true
-  validates :lname, presence: true
-  validates :fname_kana, presence: true
-  
+  validates :password, format: { with: /\A(?=.*?[a-zA-Z])(?=.*?[0-9])/ }
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ } do
+    validates :fname
+    validates :lname
+  end
+  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/ } do
+    validates :fname_kana
+    validates :lname_kana
+  end
+  validates :birthdate, presence: true
 end
