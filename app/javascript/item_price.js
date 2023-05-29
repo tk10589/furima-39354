ccaff09d4797f21959aabdaf9b40fb88672ea091
tk10589 <k1taka2014@gmail.com>
2023-05-29 19:34:05 +0900

    販売手数料や販売利益の計算実装

---
+++ app/javascript/item_price.js
@@ -0,0 +1,14 @@
+window.addEventListener('load', () => {
+  const priceInput = document.getElementById("item-price");
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+    console.log(inputValue);
+    const addTaxDom = document.getElementById("add-tax-price");
+    const addTaxPrice = Math.floor(inputValue * 0.1);
+    addTaxDom.innerHTML = addTaxPrice;
+
+    const profit = document.getElementById('profit');
+    const profitValue = Math.floor(inputValue - addTaxPrice);
+    profit.innerHTML = profitValue;
+  })
+})
