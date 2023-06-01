d55bc7cbcbf2fcfe12e3b19363278c4813e8bc2f
tk10589 <k1taka2014@gmail.com>
2023-06-01 16:59:32 +0900

    URL直接入力時、遷移処理実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
+  before_action :move_to_new, only: :edit
+  before_action :move_to_index, only: :edit
 
   def index
     @items = Item.order('created_at DESC')
@@ -40,4 +42,18 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:image, :item_name, :item_description, :category_id, :item_condition_id, :shipbase_id, :shiparea_id, :shipdate_id, :price).merge(user_id: current_user.id)
   end
+
+  def move_to_new
+    unless user_signed_in?
+      redirect_to action: :new
+    end
+  end
+
+  def move_to_index
+    @item = Item.find(params[:id])
+    unless @item.user.id == current_user.id
+      redirect_to action: :index
+    end
+  end
+
 end
