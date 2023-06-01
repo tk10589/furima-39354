0056a54489fe62bbc761e97fd484a1a9f53e2c65
tk10589 <k1taka2014@gmail.com>
2023-06-01 18:24:38 +0900

    itemコントローラー内記述修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new]
-  before_action :move_to_new, only: :edit
+  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :set_item, only: [:edit, :show, :update]
   before_action :move_to_index, only: :edit
 
   def index
@@ -21,15 +21,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path
     else
@@ -43,14 +40,11 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:image, :item_name, :item_description, :category_id, :item_condition_id, :shipbase_id, :shiparea_id, :shipdate_id, :price).merge(user_id: current_user.id)
   end
 
-  def move_to_new
-    unless user_signed_in?
-      redirect_to action: :new
-    end
+  def set_item
+    @item = Item.find(params[:id])
   end
 
   def move_to_index
-    @item = Item.find(params[:id])
     unless @item.user.id == current_user.id
       redirect_to action: :index
     end
