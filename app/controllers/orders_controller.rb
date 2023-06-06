2dafb52f92bd74c7d678946c1697d6892d384067
tk10589 <k1taka2014@gmail.com>
2023-06-06 18:23:01 +0900

    購入ページ遷移制限記述追加

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,7 +3,7 @@ class OrdersController < ApplicationController
   before_action :set_item, only: [:index, :create]
 
   def index
-    if @item.order.present?
+    if @item.order.present? || ( current_user.id == @item.user_id)
       redirect_to root_path
     else
       @order_address = OrderAddress.new
