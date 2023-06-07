3bc6c4b0ed9a7465f45d1e8b9d3b3a98b5276186
tk10589 <k1taka2014@gmail.com>
2023-06-07 11:46:37 +0900

    終了報告での遷移修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,8 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :edit, :show, :destroy]
+  before_action :authenticate_user!, only: [:new, :edit, :destroy]
   before_action :set_item, only: [:edit, :show, :update, :destroy]
   before_action :move_to_index, only: [:edit, :destroy]
-  before_action :move_to_root, only: [:show, :edit]
+  before_action :move_to_root, only: [:edit]
 
 
   def index
