8e256ad601244bd182404015a1f279b7add0740e
tk10589 <k1taka2014@gmail.com>
2023-06-15 18:15:37 +0900

    エラーメッセージ日本語化再修正６

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   has_many :items
   has_many :orders
 
-  validates :nickname, presence: : { message: "ニックネームを入力してください" }
+  validates :nickname, presence: true, message: "ニックネームを入力してください"
   validates :password, format: { with: /\A(?=.*?[a-zA-Z])(?=.*?[0-9])/, message: "パスワードは半角英字と数字の両方を含む形式で入力してください" }
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "名前は漢字、ひらがな、カタカナで入力してください" } do
     validates :fname
@@ -17,5 +17,5 @@ class User < ApplicationRecord
     validates :fname_kana
     validates :lname_kana
   end
-  validates :birthdate, presence: { message: "生年月日を入力してください" }
+  validates :birthdate, presence: true, message: "生年月日を入力してください"
 end
