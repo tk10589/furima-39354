89e8d225e863667a8d2bcc7f453229fb4b128cf9
tk10589 <k1taka2014@gmail.com>
2023-06-16 11:49:33 +0900

    エラーメッセージ日本語化再修正１０

--- app/models/delivery.rb
+++ app/models/delivery.rb
@@ -2,11 +2,11 @@ class Delivery < ApplicationRecord
   belongs_to :order
 
   with_options presence: true do
-    validates :postalcode, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'Input correctly' }
+    validates :postalcode, format: { with: /\A[0-9]{3}-[0-9]{4}\z/ }
     validates :shipcity
     validates :shipaddress
-    validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'Input only number' }
+    validates :phone_number, format: { with: /\A\d{10,11}\z/ }
   end
-  validates :shiparea_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :shiparea_id, numericality: { other_than: 1 }
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -11,11 +11,11 @@ class Item < ApplicationRecord
 
   validates :item_name, presence: true, length: { maximum: 40 }
   validates :item_description, presence: true, length: { maximum: 1000 }
-  validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :item_condition_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :shipbase_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :shiparea_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :shipdate_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :category_id, numericality: { other_than: 1 }
+  validates :item_condition_id, numericality: { other_than: 1 }
+  validates :shipbase_id, numericality: { other_than: 1 }
+  validates :shiparea_id, numericality: { other_than: 1 }
+  validates :shipdate_id, numericality: { other_than: 1 }
   with_options presence: true, format: { with: /\A[0-9]+\z/ } do
     validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
   end
